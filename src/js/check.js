b7c2a418aa561f6516fddebdd7b6a193c8f95e3b
Evgeniy Levashov <evgenii.levashov@gmail.com>
2016-10-06 21:26:18 +0300

    getMessage function: delete else, rewrite function

--- src/js/check.js
+++ src/js/check.js
@@ -1,27 +1,35 @@
 "use strict";
 
 var getMessage = function (a, b) {
-   if (typeof a === "boolean") {
-      if (a) {
-         return "Переданное GIF-изображение анимировано и содержит " +  b + " кадров";
-      } else {
-         return "Переданное GIF-изображение не анимировано";
-      }
-   } else if (typeof a === "number") {
+   
+   if (a === true) {
+      return "Переданное GIF-изображение анимировано и содержит " +  b + " кадров";
+   }
+   
+   if (a === false) {
+      return "Переданное GIF-изображение не анимировано";
+   }
+   
+   if (typeof a === "number") {
       return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
-   } else if (Array.isArray(a)) {
+   }
+   
+   if (Array.isArray(a)) {
       var amountOfRedPoints = 0;
       for (var i = 0; i < a.length; i++) {
          amountOfRedPoints += a[i];
       }
       return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
-   } else if (Array.isArray(a) && Array.isArray(b)) {
+   }
+   
+   if (Array.isArray(b) && Array.isArray(a)) {
       var artifactsSquare = 0;
       for (var i = 0; i < a.length; i++) {
          artifactsSquare += a[i] * b[i];
       }
       return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
-   } else {
-      return "Переданы некорректные данные";
+      
    }
+   
+   return "Переданы некорректные данные";
 };
