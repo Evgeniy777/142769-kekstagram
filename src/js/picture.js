269b967c1a2555334b5e0e2c3e85cb9f1001152a
Evgenii Levashov <evgenii.levashov@gmail.com>
2016-10-31 23:09:23 +0300

    modules

--- src/js/picture.js
+++ src/js/picture.js
@@ -1,4 +1,4 @@
-var getPictureElement = function (picture) {
+var getPictureElement = function(picture) {
   var template = document.querySelector('#picture-template');
   var templateContainer = 'content' in template ? template.content : template;
   var pictures = [];
@@ -11,16 +11,16 @@ var getPictureElement = function (picture) {
   pictureElement.setAttribute('href', picture.url);
   var image = new Image();
   var imageTimeout = null;
-  image.onload = function () {
+  image.onload = function() {
     clearTimeout(imageTimeout);
     pictureElement.querySelector('img').src = picture.url;
     pictureElement.querySelector('img').width = IMG_WIDTH;
     pictureElement.querySelector('img').height = IMG_HEIGHT;
   };
-  image.onerror = function () {
+  image.onerror = function() {
     pictureElement.classList.add('picture-load-failure');
   };
-  imageTimeout = setTimeout(function () {
+  imageTimeout = setTimeout(function() {
     pictureElement.classList.add('picture-load-failure');
   }, IMAGE_LOAD_TIMEOUT);
   image.setAttribute('src', picture.url);
--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,21 +1,22 @@
-module.exports = function () {
-  (function () {    
+'use strict';
+module.exports = function() {
+  (function() {
     var load = require('./load');
     var getPictureElement = require('./picture');
     var container = document.querySelector('.pictures.container');
     var PICTURES_LOAD_URL = 'http://localhost:1507/api/pictures';
 
-    var hideFilters = function () {
+    var hideFilters = function() {
       document.querySelector('.filters').classList.add('hidden');
     };
 
-    var showFilters = function () {
+    var showFilters = function() {
       document.querySelector('.filters').classList.remove('hidden');
     };
 
-    var renderPictures = function (callback) {
+    var renderPictures = function(callback) {
       hideFilters();
-      pictures.forEach(function (picture) {
+      pictures.forEach(function(picture) {
         container.appendChild(getPictureElement(picture));
       });
       showFilters();
