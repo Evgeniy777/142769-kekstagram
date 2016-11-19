70290ce04b537910a5031205b53729732a6346ea
Evgenii Levashov <evgenii.levashov@gmail.com>
2016-11-19 22:27:33 +0300

    localStorage

--- src/js/render-pictures.js
+++ src/js/render-pictures.js
@@ -49,7 +49,7 @@ module.exports = function() {
   var addMorePictures = function() {
     var scrollHeight = document.body.scrollHeight;
     var clientHeight = document.body.clientHeight;
-    if(clientHeight < scrollHeight) {
+    if(clientHeight <= scrollHeight) {
       loadPictures(activeFilter, ++pageNumber);
     }
   };
