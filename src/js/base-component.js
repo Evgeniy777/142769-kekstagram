c66b34c944d1ebd06209457d8264eac1a7cbaacf
Evgeny Levashov <evgenii.levashov@gmail.com>
2016-11-21 17:28:28 +0300

    inherit func & base component

--- src/js/base-component.js
+++ src/js/base-component.js
@@ -3,6 +3,9 @@
 var BaseComponent = function(element) {
   this.element = element;
 };
+BaseComponent.prototype.show = function(parent, element) {
+  parent.appendChild(element);
+};
 BaseComponent.prototype.remove = function() {
   this.element.parentNode.removeChild(this.element);
 };
