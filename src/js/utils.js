87f8e9f3fe89c93e37a96e36a4dee541ff567c95
Evgeny Levashov <evgenii.levashov@gmail.com>
2016-11-22 11:22:26 +0300

    8-3

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,8 +1,10 @@
 'use strict';
+
 var inherit = function(parent, child) {
   var EmptyConstructor = function() {
   };
   EmptyConstructor.prototype = parent.prototype;
   child.prototype = new EmptyConstructor();
 };
+
 module.exports = inherit;
